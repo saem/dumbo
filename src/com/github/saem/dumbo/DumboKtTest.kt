42ee2f5d01de7d7e86bd2d3852a365ac5217b01c
Saem Ghani <saemghani+github@gmail.com>
2019-04-20 17:02:50 -0700

    Got basic property based testing working. Along with program costing.

--- src/com/github/saem/dumbo/DumboKtTest.kt
+++ src/com/github/saem/dumbo/DumboKtTest.kt
@@ -6,6 +6,7 @@ import com.github.saem.dumbo.AstNode.Expression.Operation
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
 import io.kotlintest.specs.StringSpec
 
 val emptyProgram = AstNode.EmptyProgram
@@ -64,67 +65,101 @@ class AstGen : Gen<AstNode> {
 }
 
 class ExpressionGen : Gen<Expression> {
-    private val literalGen = LiteralGen()
-    private val unaryGen = OperationUnaryGen(this)
-    private val binaryGen = OperationBinaryGen(this)
-
     override fun constants() = StandardExpressions
 
     override fun random(): Sequence<Expression> =
-        Gen.oneOf(literalGen, unaryGen, binaryGen).random()
+        ExpressionGenWithLimit(Gen.choose(1, 1000).random().first()).random()
+
+    inner class ExpressionGenWithLimit(
+        private val softLimit: Int
+    ) : Gen<Expression> {
+        private val literalGen = LiteralGen()
+        private val unaryGen = OperationUnaryGen(this)
+        private val binaryGen = OperationBinaryGen(this)
+
+        private var currentSize = 0
+
+        override fun constants() = emptyList<Expression>()
+
+        override fun random(): Sequence<Expression> = when (currentSize < softLimit) {
+            true -> currentSize++.let { Gen.oneOf(literalGen, unaryGen, binaryGen) }
+            else -> literalGen
+        }.random()
+    }
 
     class LiteralGen : Gen<Literal> {
-        override fun constants() = listOf(Literal.True, Literal.False)
-        override fun random() = Gen.bind(Gen.bool()) { Literal.fromBool(it) }.random()
+        override fun constants() = StandardLiterals
+        override fun random() = Gen.bool().map { Literal.fromBool(it) }.random()
     }
 
     class OperationUnaryGen(
-        private val expressionGen: ExpressionGen
+        private val expressionGen: Gen<Expression>
     ) : Gen<Operation.Unary> {
         override fun constants() = listOf(
             Operation.Unary.Not(Literal.True),
             Operation.Unary.Not(Literal.False)
         )
 
-        override fun random(): Sequence<Operation.Unary> = Gen.bind(expressionGen)
-        { Operation.Unary.Not(it) }.random()
+        override fun random(): Sequence<Operation.Unary> =
+            expressionGen.map { Operation.Unary.Not(it) }.random()
     }
 
     class OperationBinaryGen(
-        private val expressionGen: ExpressionGen
+        private val expressionGen: Gen<Expression>
     ) : Gen<Operation.Binary> {
         override fun constants() = emptyList<Operation.Binary>()
 
         override fun random(): Sequence<Operation.Binary> =
-            Gen.oneOf(OperationBinaryAndGen(expressionGen),
+            Gen.oneOf(
+                OperationBinaryAndGen(expressionGen),
                 OperationBinaryOrGen(expressionGen)
             ).random()
     }
 
     class OperationBinaryAndGen(
-        private val expressionGen: ExpressionGen
+        private val expressionGen: Gen<Expression>
     ) : Gen<Operation.Binary.And> {
         override fun constants() = StandardAndExpressions
         override fun random(): Sequence<Operation.Binary.And> =
-            Gen.bind(expressionGen, expressionGen) { a, b -> Operation.Binary.And(a, b) }
+            Gen.bind(expressionGen, expressionGen)
+            { a, b -> Operation.Binary.And(a, b) }
                 .random()
     }
 
     class OperationBinaryOrGen(
-        private val expressionGen: ExpressionGen
+        private val expressionGen: Gen<Expression>
     ) : Gen<Operation.Binary.Or> {
         override fun constants() = StandardOrExpressions
         override fun random(): Sequence<Operation.Binary.Or> =
-            Gen.bind(expressionGen, expressionGen) { a, b -> Operation.Binary.Or(a, b) }
+            Gen.bind(expressionGen, expressionGen)
+            { a, b -> Operation.Binary.Or(a, b) }
                 .random()
     }
 
 }
 
-internal class DumboKtTest : StringSpec() {
+internal class DumboKtTest : FreeSpec() {
     init {
-        "Validate cost of programs" {
-            forAll(AstGen()) { ast: AstNode -> validate(ast) } shouldBe true
+        "Validate programs" {
+            forAll(AstGen()) { ast: AstNode -> validate(ast) }
+        }
+
+        "# Empty Programs" - {
+            "Are valid" {
+                validate(AstNode.EmptyProgram) shouldBe true
+            }
+
+            "Should have no cost" {
+                programCost(AstNode.EmptyProgram) shouldBe 0
+            }
+        }
+
+        "# Expressions - Any non-empty program" - {
+            "Are valid as long as the cost is non-negative" {
+                forAll(ExpressionGen()) {
+                    programCost(it) >= 0
+                }
+            }
         }
     }
 }
